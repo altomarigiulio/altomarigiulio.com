cf8a6e4942d5c7829a0e2c48f21c56a46230d1c2
altomarigiulio <altomari.giulio@gmail.com>
2025-07-21 22:04:19 +0200

    renaming

--- app/page.tsx
+++ app/page.tsx
@@ -137,8 +137,8 @@ export default function Personal() {
       >
         <div className="flex-1">
           <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
+            Currently studying Computer Science Engineering at the KTH Royal Institute of Technology in Stockholm, Sweden.
+            I have a passion for software development, electronics, and maths. I enjoy creating innovative solutions and exploring new ideas.
           </p>
         </div>
       </motion.section>
